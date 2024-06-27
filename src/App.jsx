510b8373cc00bf5fdcfc0b4d33d7a8fb45533d7e
Steven Hanson <stevenahanson@live.com>
2024-06-27 15:14:37 +0100

    fetch & store data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [data, setData] = useState([]);
+  
+  async function getExpenses() {
+    const response = await fetch("https://expenses-backend-mu.vercel.app/expenses", { headers: {
+      "Content-Type": "application/json",
+      Username: "Steven.Hanson" 
+    }});
+    const data = await response.json();
+
+    setData(data);
+  }
+
+  useEffect(() => {
+    getExpenses();
+  }, []);
+
+
   return (
     <div id="template-text">
       <h1>Expenses</h1>
